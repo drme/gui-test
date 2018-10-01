a0fa6a68b63e3f890c9e14322488f68ad67aa588
sarunas@p.x <sarunas@p.x>
2018-10-02 01:51:20 +0300

    other readability indexed

--- ScreenShotAnalyser/src/edu/ktu/screenshotanalyser/ScreenShotAnalyser.java
+++ ScreenShotAnalyser/src/edu/ktu/screenshotanalyser/ScreenShotAnalyser.java
@@ -6,13 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.apache.commons.io.FileUtils;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sampullara.cli.Args;
-
 import edu.ktu.screenshotanalyser.checks.CheckRequest;
 import edu.ktu.screenshotanalyser.checks.CheckResult;
 import edu.ktu.screenshotanalyser.checks.ICheck;
@@ -21,9 +18,11 @@ import edu.ktu.screenshotanalyser.context.AppContext;
 import edu.ktu.screenshotanalyser.context.DefaultContextProvider;
 import edu.ktu.screenshotanalyser.reporters.FoundTextsOnImagesReporter.AppExtractInfo;
 
-public class ScreenShotAnalyser {
+public class ScreenShotAnalyser
+{
 
-	private static void extractTextsFromImages(AnalyzerSettings request, String inputDir, String textsOutDir) {
+	private static void extractTextsFromImages(AnalyzerSettings request, String inputDir, String textsOutDir)
+	{
 		System.load(request.getCvLibrary());
 		ImageTextsExtractor extractor = ImageTextsExtractor.fromRequest(request);
 		extractor.extract(new AnalyzerRequest(inputDir, textsOutDir));
@@ -35,29 +34,34 @@ public class ScreenShotAnalyser {
 	static ICheck imageBasedChecks = MultiChecker.getImageBasedChecks();
 	static Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-	private static void runChecks(String projectName, File resourcesDir, File[] imagesDir, String outDir)
-			throws IOException {
+	private static void runChecks(String projectName, File resourcesDir, File[] imagesDir, String outDir) throws IOException
+	{
 		AppContext context = provider.getContext(resourcesDir.getAbsolutePath());
 		List<CheckResult> results = new ArrayList<CheckResult>();
 		results.addAll(Arrays.asList(appContextCheck.analyze(new CheckRequest("-"), context)));
-		for (File dir : imagesDir) {
+		for (File dir : imagesDir)
+		{
 			AppExtractInfo[] infos = textsProvider.getFiles(dir.getAbsolutePath());
-			for (AppExtractInfo info : infos) {
+			for (AppExtractInfo info : infos)
+			{
 				CheckRequest request = new CheckRequest(info.getShortFileName());
 				request.getExtractedTexts().addAll(info.getExtractedTexts());
 				results.addAll(Arrays.asList(imageBasedChecks.analyze(request, context)));
 			}
 		}
-		if (!results.isEmpty()) {
+		if (!results.isEmpty())
+		{
 			FileUtils.write(new File(outDir, "violations.json"), gson.toJson(results));
-			
+
 		}
 
 	}
 
-	public static void main(String[] args) throws Throwable {
+	public static void main(String[] args) throws Throwable
+	{
 
-		String baseDir = "D:/darbai/screenshots";
+		//String baseDir = "D:/darbai/screenshots";
+		String baseDir = "e:/2/";
 		String textsDir = baseDir + "/texts";
 		String droidbotImagesDir = baseDir + "/images";
 		String resourcesDir = baseDir + "/resources";
@@ -74,19 +78,22 @@ public class ScreenShotAnalyser {
 		DirectoriesProvider provider = new DirectoriesProvider();
 		File[] files = provider.getFiles(request.getResourcesDir());
 
-		for (File resourceDir : files) {
-			
+		for (File resourceDir : files)
+		{
+
 			final String projectname = resourceDir.getName();
 			System.out.println(projectname);
-			File[] extractedTextsDirectories = new File(textsDir).listFiles(new FileFilter() {
+			File[] extractedTextsDirectories = new File(textsDir).listFiles(new FileFilter()
+			{
 
 				@Override
-				public boolean accept(File pathname) {
+				public boolean accept(File pathname)
+				{
 					return pathname.getName().startsWith(projectname);
 				}
 			});
-			runChecks(projectname, resourceDir, extractedTextsDirectories,
-					new File(resultsOutDir, projectname).getAbsolutePath());
+			
+			runChecks(projectname, resourceDir, extractedTextsDirectories, new File(resultsOutDir, projectname).getAbsolutePath());
 		}
 	};
 
--- ScreenShotAnalyser/src/edu/ktu/screenshotanalyser/checks/semantic/SU2_TooHardToUnderstandCheck.java
+++ ScreenShotAnalyser/src/edu/ktu/screenshotanalyser/checks/semantic/SU2_TooHardToUnderstandCheck.java
@@ -3,66 +3,99 @@ package edu.ktu.screenshotanalyser.checks.semantic;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
-
+import com.ipeirotis.readability.engine.Readability;
+import com.ipeirotis.readability.enums.MetricType;
 import de.tudarmstadt.ukp.dkpro.core.readability.measure.ReadabilityMeasures;
 import edu.ktu.screenshotanalyser.checks.CheckRequest;
 import edu.ktu.screenshotanalyser.checks.CheckResult;
 import edu.ktu.screenshotanalyser.checks.ICheck;
 import edu.ktu.screenshotanalyser.context.AppContext;
 import edu.ktu.screenshotanalyser.context.AppContext.ResourceText;
-import edu.stanford.nlp.simple.Document;
-import edu.stanford.nlp.simple.Sentence;
 
-public class SU2_TooHardToUnderstandCheck implements ICheck {
-
+/**
+ * https://ipeirotis-hrd.appspot.com/
+ */
+public class SU2_TooHardToUnderstandCheck implements ICheck
+{
 	private final String type = "SU2";
-	private final double threshold;
-	private final int minWords;
+	//private final double threshold;
+	//private final int minWords;
 
-	public SU2_TooHardToUnderstandCheck(double threshold, int minWords) {
-		this.threshold = threshold;
-		this.minWords = minWords;
+	//public SU2_TooHardToUnderstandCheck(double threshold, int minWords)
+	//{
+	//	this.threshold = threshold;
+	//	this.minWords = minWords;
+	//}
+
+	public SU2_TooHardToUnderstandCheck()
+	{
+		//this(13d, 10);
 	}
 
-	public SU2_TooHardToUnderstandCheck() {
-		this(13d, 10);
-	}
-
+	
+	//TODO run on texts extracted from screenshots also, skip garbage from resource files like $1 place-holders
+	
 	@Override
-	public CheckResult[] analyze(CheckRequest request, AppContext context) {
-		final List<CheckResult> results = new ArrayList<>();
+	public CheckResult[] analyze(CheckRequest request, AppContext context)
+	{
+		List<CheckResult> results = new ArrayList<>();
 
-		for (final Entry<String, List<ResourceText>> languageDetails : context.getResources().entrySet()) {
-			final String resourceLanguage = languageDetails.getKey();
-			final String readabilityAnalysisLanguage = resourceLanguage.equals("default") ? "en" : resourceLanguage;
+		for (Entry<String, List<ResourceText>> languageDetails : context.getResources().entrySet())
+		{
+			String resourceLanguage = languageDetails.getKey();
+			String readabilityAnalysisLanguage = resourceLanguage.equals("default") ? "en" : resourceLanguage;
 
-			final ReadabilityMeasures measures = new ReadabilityMeasures(readabilityAnalysisLanguage);
-			for (final ResourceText resourceText : languageDetails.getValue()) {
+			// no indexes for other languages..
+			if ("en".equals(readabilityAnalysisLanguage))
+			{
+				//ReadabilityMeasures measures = new ReadabilityMeasures(readabilityAnalysisLanguage);
 
-				final String actualText = resourceText.getValue();
+				for (ResourceText resourceText : languageDetails.getValue())
+				{
+					String text = resourceText.getValue();
 
-				if (actualText == null) {
-					continue;
-				}
-				Document document = new Document(actualText);
-				List<Sentence> sentences = document.sentences();
-				int sentencesCount = sentences.size();
-				List<String> words = new ArrayList<>();
-				sentences.forEach(x -> words.addAll(x.words()));
-				if (words.size() < minWords) {
-					continue;
-				}
-				final double fog = measures.fog(words, sentencesCount);
-				if (fog > this.threshold) {
-					results.add(CheckResult.Nok(type,
-							String.format("Found text violating readalibity index: %s (max: %s) for text: %s", fog,
-									threshold, actualText),
-							resourceText.getFile() + "@" + resourceText.getKey(), resourceLanguage));
+					if ((text == null) || (text.isEmpty()))
+					{
+						continue;
+					}
+
+					text = text.trim();
+
+					Readability readability = new Readability(text);
+
+					double characters = readability.getMetric(MetricType.CHARACTERS);
+					double words = readability.getMetric(MetricType.WORDS);
+					double sentences = readability.getMetric(MetricType.SENTENCES);
+					
+					if ((characters > 0) && (words > 1) && (sentences > 0)) // no 1 word sentences are allowed
+					{
+					double ari = readability.getMetric(MetricType.ARI);
+					double colemanLiau = readability.getMetric(MetricType.COLEMAN_LIAU);
+					double complexWords = readability.getMetric(MetricType.COMPLEXWORDS);
+					double fleschKincaid = readability.getMetric(MetricType.FLESCH_KINCAID);
+					double flecshReading = readability.getMetric(MetricType.FLESCH_READING);
+					double gunningFog = readability.getMetric(MetricType.GUNNING_FOG);
+					double smog = readability.getMetric(MetricType.SMOG);
+					double smogIndex = readability.getMetric(MetricType.SMOG_INDEX);
+					double syllabes = readability.getMetric(MetricType.SYLLABLES);
+
+					// TODO: skip garbage texts, combine index to somethig useable... select index that wprks with single sentence...
+					
+					if ((flecshReading > 70) && (smogIndex > 10))
+					{
+						System.out.println("[" + text + "] - FleschReadingEase: " + flecshReading + ", ari: " + ari);
+
+						results.add(CheckResult.Nok(type, String.format("Fairly difficult to read text: %s", text), resourceText.getFile() + "@" + resourceText.getKey(), resourceLanguage));
+					}
+					}
 				}
 			}
 		}
 
+		/*
+		 * Document document = new Document(actualText); List<Sentence> sentences = document.sentences(); int sentencesCount = sentences.size(); List<String> words = new ArrayList<>(); sentences.forEach(x -> words.addAll(x.words())); if (words.size() < minWords) { continue; } final double fog = measures.fog(words, sentencesCount); if (fog > this.threshold) { results.add(CheckResult.Nok(type, String.format("Found text violating readalibity index: %s (max: %s) for text: %s", fog, threshold, actualText), resourceText.getFile() + "@" + resourceText.getKey(), resourceLanguage)); }
+		 */
+
 		return results.toArray(new CheckResult[0]);
 	}
-
 }
