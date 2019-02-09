8586d5cbabfb52ca840ffda0c6395fa908f7f028
rd.greta@gmail.com <gretard@users.noreply.github.com>
2019-02-09 11:22:06 +0200

    Add ability to select area on image

--- research/experiments/tools/ImageSorter/DefectsViewer/MainWindow.xaml.cs
+++ research/experiments/tools/ImageSorter/DefectsViewer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Brushes = System.Windows.Media.Brushes;
+
 
 namespace DefectsViewer
 {
@@ -24,8 +27,13 @@ namespace DefectsViewer
           //  @"E:\darbai\sh\OneDrive_1_2-8-2019\app-screenshots\_results_\";
 
         private ImagesManager sorter = new ImagesSorter(baseFolder);
+        // True when we're selecting a rectangle.
+        private bool IsSelecting = false;
 
-		public MainWindow()
+        // The area we are selecting.
+      
+
+        public MainWindow()
 		{
 			InitializeComponent();
 
@@ -54,28 +62,65 @@ namespace DefectsViewer
 		}
 
 		private void ShowActiveImage()
-		{
-			var image = new BitmapImage(new Uri(this.sorter.ActiveImage.FullName));
+        {
+            var image = new BitmapImage(new Uri(this.sorter.ActiveImage.FullName));
 
-			this.ImageView.Source = image;
+            ShowImage(image);
+        }
 
-			var p = PresentationSource.FromVisual(this);
-
-			if (null != p)
-			{
-				Matrix m = p.CompositionTarget.TransformToDevice;
-				double dx = m.M11;
-				double dy = m.M22;
-
-				double h = this.ImageView.Height;
+        private void ShowImage(BitmapImage src)
+        {
 
 
 
-				this.Title = this.sorter.Status + " " + h + " " + dy;
-			}
-		}
 
-		private void MarkDefect(String defect)
+
+            ImageSource s = src;
+         
+            System.Diagnostics.Debug.WriteLine(X0 + " " + Y0 + "  -  " + X1 + " " + Y1);
+            var h = Math.Abs(Y1 - Y0);
+            var w = Math.Abs(X1 - X0);
+           if (X0 > 0 && Y0 > 0 && h > 0 && w > 0 && this.H > 0 && this.W > 0)
+            {
+                var dh = src.PixelHeight / this.H;
+                var dw = src.PixelWidth / this.W;
+                 var r = new Rect(X0 * dw, Y0 * dh, w * dw, h * dh);
+                 System.Diagnostics.Debug.WriteLine(r);
+                 DrawingVisual dv = new DrawingVisual();
+                 using (DrawingContext dc = dv.RenderOpen())
+                 {
+                     dc.DrawImage(src, new Rect(0, 0, src.PixelWidth, src.PixelHeight));
+                     dc.DrawRectangle(null, new Pen(new SolidColorBrush(Color.FromRgb(255,0,0)), 1.5), r);
+                 }
+
+                 RenderTargetBitmap rtb = new RenderTargetBitmap(src.PixelWidth, src.PixelHeight, 96, 96, PixelFormats.Pbgra32);
+                 rtb.Render(dv);
+                 s = rtb;
+                 System.Diagnostics.Debug.WriteLine("Updated");
+            }
+
+          
+
+
+            this.ImageView.Source = s;
+            var p = PresentationSource.FromVisual(this);
+
+            if (null != p)
+            {
+                Matrix m = p.CompositionTarget.TransformToDevice;
+                var dx = m.M11;
+                var dy = m.M22;
+
+                double hh = this.ImageView.Height;
+
+
+
+                this.Title = this.sorter.Status + " " + hh + " " + dy;
+            }
+            
+        }
+
+        private void MarkDefect(String defect)
 		{
 			this.sorter.MarkBad(defect);
 			ShowActiveImage();
@@ -146,19 +191,30 @@ namespace DefectsViewer
 			MarkDefect(DefectTypes.BadColors);
 		}
 
-        private void ImageView_MouseUp(object sender, MouseButtonEventArgs e)
-        {
 
+            
+
+        private void ImageView_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Point p = e.GetPosition(this.ImageView);
+            this.X1 = p.X;
+            this.Y1 = p.Y;
+            this.H = this.ImageView.ActualHeight;
+            this.W = this.ImageView.ActualWidth;
+            ShowActiveImage();
         }
 
-        private void ImageView_MouseDown(object sender, MouseButtonEventArgs e)
+        private void ImageView_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            System.Windows.Point p = e.GetPosition(this.ImageView);
+            this.X0 = p.X;
+            this.Y0 = p.Y;
+            this.H = this.ImageView.ActualHeight;
+            this.W = this.ImageView.ActualWidth;
+            ShowActiveImage();
         }
-
-        private void ImageView_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-
-        }
+        private double X0, X1, Y0, Y1 = -1;
+        private double H = -1;
+        private double W = -1;
     }
 }
